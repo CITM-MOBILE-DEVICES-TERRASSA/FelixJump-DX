707e6a4dd2a38e27eabab74a33c9642eb72fa387
guillemal <guillem.alqueza@estudiantat.upc.edu>
2024-11-05 19:51:50 +0100

    Progress bar

---
+++ Felix Jump/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    [SerializeField] Image barImage;
+
+    private void Start()
+    {
+        StartCoroutine(StartCountdown(2.0f));
+    }
+
+    private IEnumerator StartCountdown(float duration)
+    {
+        float timeRemaining = duration;
+
+        while (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            barImage.fillAmount = timeRemaining / duration;
+
+            yield return null;
+        }
+
+        ScenesManager.Instance.LoadScene("GameSelector");
+    }
+}
