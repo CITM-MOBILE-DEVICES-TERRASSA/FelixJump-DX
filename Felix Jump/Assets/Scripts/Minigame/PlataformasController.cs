a9a0b6c287b769443c0e781114aff9f47c9cb399
Aleix3 <Aleixbotella.1@gmail.com>
2024-11-13 08:38:49 +0100

    Pinchos Metidos

--- Felix Jump/Assets/Scripts/Minigame/PlataformasController.cs
+++ Felix Jump/Assets/Scripts/Minigame/PlataformasController.cs
@@ -10,6 +10,8 @@ public class PlataformaController : MonoBehaviour
     public GameObject plataforma1Hole;
     public GameObject plataforma2Hole;
     public GameObject ballPrefab; // Add a reference to the ball prefab
+    public GameObject plataformaPinchos1Hole;
+    public GameObject plataformaPinchos2Hole;
 
     private GameObject plataformaToSpawn;
 
@@ -20,32 +22,46 @@ public class PlataformaController : MonoBehaviour
 
     private float lastOrientationY = 0;
 
- 
-   
     // Start is called before the first frame update
     void Start()
     {
-        
         //Plataformas
-        for(int i = 0; i<numeroPlataforma; i++)
+        for (int i = 0; i < numeroPlataforma; i++)
         {
-            plataformaToSpawn = (Random.Range(0, 2) == 0) ? plataforma1Hole : plataforma2Hole;
+            // Elegir plataforma al azar entre las cuatro opciones
+            int plataformaAleatoria = Random.Range(0, 4);
+            if (plataformaAleatoria == 0)
+            {
+                plataformaToSpawn = plataforma1Hole;
+            }
+            else if (plataformaAleatoria == 1)
+            {
+                plataformaToSpawn = plataforma2Hole;
+            }
+            else if (plataformaAleatoria == 2)
+            {
+                plataformaToSpawn = plataformaPinchos1Hole;
+            }
+            else
+            {
+                plataformaToSpawn = plataformaPinchos2Hole;
+            }
 
+            // Asignar rotación aleatoria manteniendo una diferencia mínima de 45 grados
             float rotationY = Random.Range(45, 315);
-            while( Mathf.Abs(rotationY - lastOrientationY) < 45)
+            while (Mathf.Abs(rotationY - lastOrientationY) < 45)
             {
                 rotationY = Random.Range(45, 315);
             }
             lastOrientationY = rotationY;
 
-            Instantiate(plataformaToSpawn, new Vector3(0, startingY + (distanciaSpawn * (i+1)), 0), Quaternion.Euler(0, rotationY, 0), CylinderController.instance.cylinder.transform);
+            Instantiate(plataformaToSpawn, new Vector3(0, startingY + (distanciaSpawn * (i + 1)), 0), Quaternion.Euler(0, rotationY, 0), CylinderController.instance.cylinder.transform);
         }
 
+        // Instanciar meta final
         Instantiate(metaFinal, new Vector3(0, startingY + (distanciaSpawn * (numeroPlataforma + 1)), 0), Quaternion.identity, CylinderController.instance.cylinder.transform);
 
-
-
-        //Pelota
+        // Instanciar pelota
         GameObject ball = Instantiate(ballPrefab, new Vector3(0, 0.4f, -0.7f), Quaternion.identity);
         Rigidbody rb = ball.GetComponent<Rigidbody>();
         if (rb != null)
@@ -58,18 +74,6 @@ public class PlataformaController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //contador += Time.deltaTime;
-        //if (contador > 5)
-        //{
-        //    GameObject plataforma = Instantiate(plataforma1Hole, CylinderController.instance.cylinder.transform);
-        //    plataformas.Add(plataforma.transform);
-        //    contador = 0;
-        //}
-
-
-        //foreach(Transform plataforma in plataformas)
-        //{
-        //    plataforma.transform.position = new Vector3(0, plataforma.transform.position.y - (movementSpeed * Time.deltaTime), 0);
-        //}
+        // Aquí puedes añadir lógica adicional si es necesario.
     }
 }
